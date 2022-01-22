1395ecab298b68403b5b4def0a75983e3fc900da
Nicolas Friedli <nicolas.friedli@gmail.com>
2022-01-22 17:57:10 +0100

    Formule haversine sans dépendances!

--- distances.go
+++ distances.go
@@ -7,10 +7,9 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"math"
 	"os"
 	"sort"
-
-	"github.com/connerdouglass/go-geo"
 )
 
 // définition pour reprise du json général
@@ -30,6 +29,40 @@ type Cible struct {
 	Distance float64 `json:"distance"`
 }
 
+// Calcul de distance par haversine
+// source: https://gist.github.com/cdipaolo/d3f8db3848278b49db68
+// haversin(θ) function
+func hsin(theta float64) float64 {
+	return math.Pow(math.Sin(theta/2), 2)
+}
+
+// Distance function returns the distance (in meters) between two points of
+//     a given longitude and latitude relatively accurately (using a spherical
+//     approximation of the Earth) through the Haversin Distance Formula for
+//     great arc distance on a sphere with accuracy for small distances
+//
+// point coordinates are supplied in degrees and converted into rad. in the func
+//
+// distance returned is METERS!!!!!!
+// http://en.wikipedia.org/wiki/Haversine_formula
+func Distance(lat1, lon1, lat2, lon2 float64) float64 {
+	// convert to radians
+	// must cast radius as float to multiply later
+	var la1, lo1, la2, lo2, r float64
+	la1 = lat1 * math.Pi / 180
+	lo1 = lon1 * math.Pi / 180
+	la2 = lat2 * math.Pi / 180
+	lo2 = lon2 * math.Pi / 180
+
+	r = 6378.1 // Earth radius in km
+
+	// calculate
+	h := hsin(la2-la1) + math.Cos(la1)*math.Cos(la2)*hsin(lo2-lo1)
+
+	return 2 * r * math.Asin(math.Sqrt(h))
+}
+
+// Le programme principal
 func main() {
 
 	// ouverture, validation et fermeture du json général
@@ -51,23 +84,13 @@ func main() {
 	// parcours des paroisses en tant que source
 	for i := 0; i < len(paroisses); i++ {
 
-		a := geo.Coordinate{
-			Latitude:  paroisses[i].Latitude,
-			Longitude: paroisses[i].Longitude,
-		}
-
 		var cibles []Cible
 
 		// parcours de chaque paroisse cible pour une source donnée
 		for j := 0; j < len(paroisses); j++ {
 
 			// calcul de la distance source-cible
-			b := geo.Coordinate{
-				Latitude:  paroisses[j].Latitude,
-				Longitude: paroisses[j].Longitude,
-			}
-
-			distance := geo.DistanceBetween(a, b).Kilometers()
+			distance := Distance(paroisses[i].Latitude, paroisses[i].Longitude, paroisses[j].Latitude, paroisses[j].Longitude)
 
 			// création de la cible et ajout à la liste
 			var cible Cible
--- go.mod
+++
@@ -1,5 +0,0 @@
-module distances
-
-go 1.17
-
-require github.com/connerdouglass/go-geo v1.0.1
