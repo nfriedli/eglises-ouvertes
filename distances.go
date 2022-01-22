4fb2d3a6aea9d55fb4c42006165d12ead55fb7e3
Nicolas Friedli <nicolas@theologique.ch>
2022-01-22 12:25:44 +0100

    Quelques commentaires.

--- distances.go
+++ distances.go
@@ -1,3 +1,6 @@
+// Calcul des distances entre les Églises ouvertes.
+// Nécessite une première compilation par Hugo.
+// Crée un fichier distances.json dans le répertoire data.
 package main
 
 import (
@@ -10,6 +13,7 @@ import (
 	"github.com/connerdouglass/go-geo"
 )
 
+// définition pour reprise du json général
 type Paroisses []struct {
 	Title     string  `json:"title"`
 	URL       string  `json:"url"`
@@ -18,6 +22,7 @@ type Paroisses []struct {
 	Longitude float64 `json:"longitude"`
 }
 
+// définition d'une paroisse cible
 type Cible struct {
 	Title    string  `json:"nom"`
 	URL      string  `json:"url"`
@@ -27,6 +32,7 @@ type Cible struct {
 
 func main() {
 
+	// ouverture, validation et fermeture du json général
 	jsonFile, err := os.Open("public/index.json")
 	if err != nil {
 		fmt.Println(err)
@@ -42,6 +48,7 @@ func main() {
 
 	distances := make(map[string][]Cible)
 
+	// parcours des paroisses en tant que source
 	for i := 0; i < len(paroisses); i++ {
 
 		a := geo.Coordinate{
@@ -51,14 +58,18 @@ func main() {
 
 		var cibles []Cible
 
+		// parcours de chaque paroisse cible pour une source donnée
 		for j := 0; j < len(paroisses); j++ {
 
+			// calcul de la distance source-cible
 			b := geo.Coordinate{
 				Latitude:  paroisses[j].Latitude,
 				Longitude: paroisses[j].Longitude,
 			}
 
 			distance := geo.DistanceBetween(a, b).Kilometers()
+
+			// création de la cible et ajout à la liste
 			var cible Cible
 
 			cible.Title = paroisses[j].Title
@@ -66,20 +77,23 @@ func main() {
 			cible.Canton = paroisses[j].Canton
 			cible.Distance = distance
 
-			//println("-> ", cible.Title, cible.Distance)
-
 			cibles = append(cibles, cible)
 		}
 
+		// tri des cibles par distances
 		sort.SliceStable(cibles, func(a, b int) bool {
 			return cibles[a].Distance < cibles[b].Distance
 		})
+
+		// raccourcissement de la liste
 		cibles = cibles[1:6]
+
+		// ajoute à la table générale
 		distances[paroisses[i].URL] = cibles
 
 	}
 
-	// output, err := json.MarshalIndent(distances, "", "  ")
+	// création, validation et écriture du json de sortie
 	output, err := json.Marshal(distances)
 	if err != nil {
 		fmt.Println(err)
