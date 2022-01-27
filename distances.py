c904ae1fea48e095a2de1dd1558db2ad2d54fe70
Nicolas Friedli <nicolas@theologique.ch>
2022-01-27 09:43:17 +0100

    Test en Python (just for fun).

---
+++ distances.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+
+# Il est recommandé d'utiliser les script de conversion en go.
+# Celui-ci ne sera pas forcément tenu à jour.
+
+import json
+from math import radians, cos, sin, asin, sqrt
+import operator
+
+# source: https://stackoverflow.com/questions/4913349/haversine-formula-in-python-bearing-and-distance-between-two-gps-points
+def haversine(lon1, lat1, lon2, lat2):
+    """
+    Calculate the great circle distance in kilometers between two points 
+    on the earth (specified in decimal degrees)
+    """
+    # convert decimal degrees to radians 
+    lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
+
+    # haversine formula 
+    dlon = lon2 - lon1 
+    dlat = lat2 - lat1 
+    a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
+    c = 2 * asin(sqrt(a)) 
+    r = 6371 # Radius of earth in kilometers. Use 3956 for miles. Determines return value units.
+    return c * r
+
+with open("public/index.json") as source:
+    paroisses = json.load(source)
+    
+# tableau des distances qui sera converti en json
+distances = {}
+
+# on parcourt les paroisses sources
+for i in paroisses:
+
+    # on crée un liste de cibles pour chaque paroisse
+    cibles = []
+
+    # on parcourt les paroisses cibles (potentielles)
+    for j in paroisses:
+        distance = haversine(i["longitude"], i["latitude"], j["longitude"], j["latitude"])
+
+        # pour chaque cible, on crée une donnée structurée
+        cible = {}
+        cible["nom"] = j["title"]
+        cible["url"] = j["url"]
+        cible["canton"] = j["canton"]
+        cible["distance"] = distance
+
+        # on ajoute la cible à la liste de cibles
+        cibles.append(cible)
+        
+    # on trie les cibles par distances    
+    cibles.sort(key=operator.itemgetter("distance"))
+
+    # on coupe la liste en supprimant le doublet (source = cible) et en prenant les 5 premières cibles
+    cibles = cibles[1:6]
+
+    # on ajoute les cibles aux distances (avec source comme clé)
+    distances[i["url"]] = cibles
+
+with open("data/distances.json", "w", encoding="utf8") as output:
+    json.dump(distances, output, ensure_ascii=False, sort_keys=True)
