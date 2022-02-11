dcbf6769073a799d90bb27132b0f878f9dbd3452
Nicolas Friedli <nicolas@theologique.ch>
2022-02-11 14:08:03 +0100

    Carte en 13 lignes de code...

---
+++ carte-folium.py
@@ -0,0 +1,13 @@
+import json
+import folium
+
+with open("public/index.json") as source:
+    paroisses = json.load(source)
+
+m = folium.Map(location=[46.96661, 6.86231])
+
+for i in paroisses:
+    folium.Marker([i["latitude"], i["longitude"]],
+    popup=i["title"]).add_to(m)
+
+m.save("public/carte.html")
